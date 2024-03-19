9f2edc3b242d58a4a898e13f0fd6e5a855e7af08
Udara Dananjaya <b.udara.dananjaya.dev@gmail.com>
2024-03-20 00:03:49 +0530

    Update files

--- commands.bash
+++ commands.bash
@@ -7,3 +7,6 @@ whereis php
 sudo nano /etc/systemd/system/laravel.service
 sudo service laravel restart
 sudo service laravel status
+
+systemctl --user enable example.service
+systemctl --user start example.service
